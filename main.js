57a95d4961537060847a0aed838a8b12d9937816
AndreaPanzeriDev <andpanz@libero.it>
2022-10-18 16:26:53 +0200

    input + creazione password

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+const firstname = prompt('Inserisci il tuo nome');
+console.log(firstname);
+
+const lastname = prompt('Inserisci il tuo cognome');
+console.log(lastname);
+
+
+const color = prompt("Inserisci il tuo colore preferito");
+console.log(color);
+
+let age = prompt("Inserisci la tua età");
+console.log(age);
+
+
+let pw = firstname + lastname + color + age;
+console.log(pw);
